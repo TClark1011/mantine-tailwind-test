c80e29cb6de2fd32d778d039f8e5bf77da0833ba
Thomas Clark <thomas.clark@bigtincan.com>
2023-10-18 17:28:56 +1100

    setup build analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -7,4 +9,4 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {};
 
-export default config;
+export default withBundleAnalyzer()(config);
